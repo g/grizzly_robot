ef4ea8300422e0b5f710581226d3e924a0a320ed
Mike Purvis <mpurvis@clearpathrobotics.com>
2013-11-09 15:08:21 -0500

    Build fixes for motor fanout node.

--- grizzly_base/src/motor_fanout.cpp
+++ grizzly_base/src/motor_fanout.cpp
@@ -23,10 +23,10 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCL
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <ros/ros.h>
-#include <grizzly_msgs/Drive.h>
-#include <roboteq_msgs/Command.h>
-#include <roboteq_msgs/Feedback.h>
+#include "ros/ros.h"
+#include "grizzly_msgs/Drive.h"
+#include "roboteq_msgs/Command.h"
+#include "roboteq_msgs/Feedback.h"
 
 #include <boost/foreach.hpp>
 #include <stdexcept>
