10e6c2ecae47e92e8d0ca882786f5b381df225af
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2018-06-06 17:29:05 -0400

    Updated Udev, maintainer and CAN bridge interface.

--- grizzly_bringup/scripts/can_up.sh
+++ grizzly_bringup/scripts/can_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-socat udp4-datagram:255.255.255.255:11412,bind=:11412,range=192.168.131.0/24,broadcast,so-bindtodevice=br0 pty,link=/dev/ttycan0 &
+socat udp4-datagram:255.255.255.255:11412,bind=:11412,range=192.168.131.0/24,broadcast,so-bindtodevice=enp1s0 pty,link=/dev/ttycan0 &
 #socat udp4-datagram:224.0.0.1:11412,bind=:11412,range=192.168.131.0/24,ip-add-membership=224.0.0.1:192.168.131.1 pty,link=/dev/ttycan0 &
 sleep 1
 slcand -o -c -F -s5 /dev/ttycan0 vcan0 &
